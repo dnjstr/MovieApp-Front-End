7d6057a2e1d4288bfb68fb55f40b3cb1e757953d
dnjtr <antonio.denjester28@gmail.com>
2025-02-09 16:32:46 +0800

    Minimal Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
@@ -8,14 +7,15 @@ import MovieDetail from "./components/MovieDetail";
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="bg-gray-900 text-white min-h-screen">
-        <Navbar />
-        <div className="pt-16 p-6">
-          <Routes>
-            <Route path="/" element={<><HeroSection /><MovieListSection /></>} />
-            <Route path="/movies/:id" element={<MovieDetail />} />
-          </Routes>
-        </div>
+      <Navbar />
+      <div className="bg-gray-900 text-white min-h-screen p-6">
+        <Routes>
+          <Route path="/" element={<><HeroSection /><MovieListSection /></>} />
+          <Route path="/popular" element={<div>New & Popular Page</div>} />
+          <Route path="/genre" element={<div>Genre Page</div>} />
+          <Route path="/my-list" element={<div>My List Page</div>} />
+          <Route path="/movies/:id" element={<MovieDetail />} />
+        </Routes>
       </div>
     </Router>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FaHome, FaSearch, FaBars } from 'react-icons/fa';
 
 const Navbar: React.FC = () => {
@@ -6,18 +7,20 @@ const Navbar: React.FC = () => {
     <nav className="flex items-center justify-between p-4 fixed w-full bg-black bg-opacity-75 z-50">
       {/* Logo */}
       <div className="flex items-center space-x-2">
-        <img src="/2972234.png" alt="JustWatch Logo" className="h-8" />
+        <Link to="/">
+          <img src="/2972234.png" alt="JustWatch Logo" className="h-8 cursor-pointer" />
+        </Link>
       </div>
 
       {/* Navigation Icons */}
       <div className="flex space-x-6 text-white">
-        <button className="flex items-center space-x-2 hover:text-yellow-400">
+        <Link to="/" className="flex items-center space-x-2 hover:text-yellow-400">
           <FaHome size={20} />
           <span className="hidden md:inline">Home</span>
-        </button>
-        <button className="hover:text-yellow-400">New & Popular</button>
-        <button className="hover:text-yellow-400">Genre</button>
-        <button className="hover:text-yellow-400">My List</button>
+        </Link>
+        <Link to="/popular" className="hover:text-yellow-400">New & Popular</Link>
+        <Link to="/genre" className="hover:text-yellow-400">Genre</Link>
+        <Link to="/my-list" className="hover:text-yellow-400">My List</Link>
       </div>
 
       {/* Search Bar */}
@@ -32,7 +35,7 @@ const Navbar: React.FC = () => {
 
       {/* Right Side */}
       <div className="flex items-center space-x-4">
-        <button className="bg-gray-700 px-4 py-2 rounded-md">Sign In</button>
+        <button className="bg-red-700 px-4 py-2 rounded-md">Sign In</button>
         <FaBars size={24} className="text-white cursor-pointer" />
       </div>
     </nav>
