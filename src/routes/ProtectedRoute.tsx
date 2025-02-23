30b2320f694b7bd8536963d16f73bd3a0efb88c8
dnjtr <antonio.denjester28@gmail.com>
2025-02-23 14:53:30 +0800

    Commented out the ProtectedRoute file

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,16 +1,16 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext"; 
+// import { Navigate } from "react-router-dom";
+// import { useAuth } from "../../context/AuthContext"; 
 
-const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { isAuthenticated, loading } = useAuth();
+// const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+//   const { isAuthenticated, loading } = useAuth();
 
-  if (loading) return null; // Prevent redirection before loading is complete
+//   if (loading) return null; // Prevent redirection before loading is complete
 
-  if (!isAuthenticated) {
-    return <Navigate to="/sign-in" replace />;
-  }
+//   if (!isAuthenticated) {
+//     return <Navigate to="/sign-in" replace />;
+//   }
 
-  return children;
-};
+//   return children;
+// };
 
-export default ProtectedRoute;
+// export default ProtectedRoute;
