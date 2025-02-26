a1deea61ec96b6c5957604d531947424303e1612
Gshadow2005 <aninoglenn@gmail.com>
2025-02-26 17:06:29 +0800

    Scroll to top fixed Responsivness

--- src/components/pages/faq.tsx
+++ src/components/pages/faq.tsx
@@ -297,15 +297,26 @@ const FAQ = () => {
         </div>
       </div>
 
-      {/* scrol top*/}
+      {/* desktop scrol top*/}
       <button
-        className={`fixed stbutton bottom-8 border border-gray-700 bg-orange-600 text-white p-4 rounded-full transition-opacity duration-300 hover:bg-orange-800 ${
+        className={`fixed lg:block hidden stbutton bottom-8 border border-gray-700 bg-orange-600 text-white p-4 rounded-full transition-opacity duration-300 hover:bg-orange-800 ${
           showScrollTop ? 'opacity-100' : 'opacity-0 pointer-events-none'
         }`}
         onClick={scrollToTop}
       >
         ↑
       </button>
+      
+      {/* mobile scroll top*/}
+      <button
+        className={`fixed lg:hidden block right-8  bottom-8 border border-gray-700 bg-orange-600 text-white p-4 rounded-full transition-opacity duration-300 hover:bg-orange-800 ${
+          showScrollTop ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        }`}
+        onClick={scrollToTop}
+      >
+        ↑
+      </button>
+
     </div>
   );
 };
