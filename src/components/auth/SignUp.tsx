bcc7d461bd137818f245c5c82bb8e5748e1c6da1
Gshadow2005 <aninoglenn@gmail.com>
2025-03-01 23:48:16 +0800

    Partial Design Changes on Sign IN-UP

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -4,7 +4,7 @@ import SignUpHeader from "./sign-up/SignUpHeader";
 
 const SignUp: React.FC = () => {
     return (
-        <div className="flex justify-center items-center min-h-screen relative">
+        <div className="flex justify-center items-center min-h-screen">
             <div className="signin-page-container absolute top-0 left-0 w-full h-full z-0"></div>
             <div className="flex z-10">
                 <div className="bg-black bg-opacity-25 p-8 rounded-lg shadow-lg relative w-[380px] px-4 border border-gray-700">
