d6c23a316418c6f67e9bf65483b02e797137775b
dnjtr <antonio.denjester28@gmail.com>
2025-03-05 17:45:13 +0800

    Changes in API

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://54.252.243.166/api",
+    baseURL: "https://54.252.243.166/api",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
